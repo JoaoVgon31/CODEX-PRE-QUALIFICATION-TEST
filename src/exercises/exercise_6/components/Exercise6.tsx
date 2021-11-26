ce7e96b3069587f577ac4cd5f3846c1e961a77e3
joao.goncalves@conpec.com.br <joao.goncalves@conpec.com.br>
2021-11-26 19:11:19 -0300

    Created page to show the server working

--- src/exercises/exercise_6/components/Exercise6.tsx
+++ src/exercises/exercise_6/components/Exercise6.tsx
@@ -1,7 +1,35 @@
+import { Button } from '@material-ui/core';
 import React from 'react';
 
 function Exercise6(): JSX.Element {
-	return <div className="flex justify-center align-center w-full-screen h-full-screen">Exercício Não Completado</div>;
+	return (
+		<div className="flex justify-center align-center w-full-screen h-full-screen">
+			<div className="flex flex-col justify-center align-center">
+				<div className="margin-y-small">
+					Link para requisição:{' '}
+					<a
+						href="https://us-central1-codex-pre-qualification-test.cloudfunctions.net/getCurrentDateTime"
+						target="_blank"
+						rel="noreferrer"
+					>
+						https://us-central1-codex-pre-qualification-test.cloudfunctions.net/getCurrentDateTime
+					</a>
+				</div>
+				<div className="flex w-full justify-evenly align-center">
+					<Button variant="outlined" href="/">
+						Voltar
+					</Button>
+					<Button
+						variant="contained"
+						href="https://us-central1-codex-pre-qualification-test.cloudfunctions.net/getCurrentDateTime"
+						target="_blank"
+					>
+						Executar
+					</Button>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Exercise6;
