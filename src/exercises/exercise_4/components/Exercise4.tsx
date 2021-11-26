373015759dfc40dcc95afbbfdef3a1dc040813ab
joao.goncalves@conpec.com.br <joao.goncalves@conpec.com.br>
2021-11-26 13:44:21 -0300

    Added firebase firestore to project

--- src/exercises/exercise_4/components/Exercise4.tsx
+++ src/exercises/exercise_4/components/Exercise4.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firebase from '../../../config/firebase';
 
 function Exercise4(): JSX.Element {
 	return <div className="flex justify-center align-center w-full-screen h-full-screen">Exercício Não Completado</div>;
