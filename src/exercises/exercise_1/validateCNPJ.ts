3f654f7a0792fddbca11a22496e19cfb3b71491f
joao.goncalves@conpec.com.br <joao.goncalves@conpec.com.br>
2021-11-25 22:36:24 -0300

    Implemented function to check digits

--- src/exercises/exercise_1/validateCNPJ.ts
+++ src/exercises/exercise_1/validateCNPJ.ts
@@ -25,7 +25,22 @@ function validateDigitsNumber(CNPJ: Array<number>): boolean {
 }
 
 function validateCheckDigits(CNPJ: Array<number>): boolean {
-	return true;
+	const checkDigits = [0, 0];
+
+	checkDigits[0] = 5 * CNPJ[0] + 4 * CNPJ[1] + 3 * CNPJ[2] + 2 * CNPJ[3];
+	checkDigits[0] += 9 * CNPJ[4] + 8 * CNPJ[5] + 7 * CNPJ[6] + 6 * CNPJ[7];
+	checkDigits[0] += 5 * CNPJ[8] + 4 * CNPJ[9] + 3 * CNPJ[10] + 2 * CNPJ[11];
+	checkDigits[0] = 11 - (checkDigits[0] % 11);
+	checkDigits[0] = checkDigits[0] >= 10 ? 0 : checkDigits[0];
+
+	checkDigits[1] = 6 * CNPJ[0] + 5 * CNPJ[1] + 4 * CNPJ[2] + 3 * CNPJ[3];
+	checkDigits[1] += 2 * CNPJ[4] + 9 * CNPJ[5] + 8 * CNPJ[6] + 7 * CNPJ[7];
+	checkDigits[1] += 6 * CNPJ[8] + 5 * CNPJ[9] + 4 * CNPJ[10] + 3 * CNPJ[11];
+	checkDigits[1] += 2 * CNPJ[12];
+	checkDigits[1] = 11 - (checkDigits[1] % 11);
+	checkDigits[1] = checkDigits[1] >= 10 ? 0 : checkDigits[1];
+
+	return checkDigits[0] === CNPJ[12] && checkDigits[1] === CNPJ[13];
 }
 
 function validateCNPJ(CNPJ: string): [boolean, Messages] {
@@ -38,7 +53,7 @@ function validateCNPJ(CNPJ: string): [boolean, Messages] {
 	if (!validateDigitsNumber(onlyNumbersCNPJArray)) {
 		return [false, Messages.INVALID_DIGITS_NUMBER];
 	}
-	if (!validateDigitsNumber(onlyNumbersCNPJArray)) {
+	if (!validateCheckDigits(onlyNumbersCNPJArray)) {
 		return [false, Messages.INVALID_CHECK_DIGITS];
 	}
 
