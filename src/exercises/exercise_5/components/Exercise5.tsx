59ce357a8f2fdc6353cd289e7fc93da498baff82
joao.goncalves@conpec.com.br <joao.goncalves@conpec.com.br>
2021-11-26 23:45:49 -0300

    Display date and time informations

--- src/exercises/exercise_5/components/Exercise5.tsx
+++ src/exercises/exercise_5/components/Exercise5.tsx
@@ -1,22 +1,33 @@
 import { Button } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import queryWorldClockApi from '../queryWorldClockApi';
 
 function Exercise5(): JSX.Element {
+	const [currentUTCDateTime, setCurrentUTCDateTime] = useState('');
+	const [currentLocalDateTime, setCurrentLocalDateTime] = useState('');
+	const [currentBRDateTime, setCurrentBRDateTime] = useState('');
+
 	const handleGetCurrentDateTime = async (): Promise<void> => {
 		const currentDateTimeUTC = await queryWorldClockApi();
-		console.log(currentDateTimeUTC);
+		setCurrentUTCDateTime(new Date(currentDateTimeUTC).toUTCString);
+		setCurrentLocalDateTime(new Date(currentDateTimeUTC).toLocaleString);
+		setCurrentBRDateTime(new Date(currentDateTimeUTC).toLocaleString('pt-br'));
 	};
 
 	return (
 		<div className="flex justify-center align-center w-full-screen h-full-screen">
-			<div className="flex justify-evenly align-center w-1/4-screen">
-				<Button variant="outlined" href="/">
-					Voltar
-				</Button>
-				<Button variant="contained" onClick={handleGetCurrentDateTime}>
-					Executar
-				</Button>
+			<div className="flex flex-col justify-center align-center w-1/4-screen">
+				<div className="flex justify-center">Data e hora UTC: {currentUTCDateTime}</div>
+				<div className="flex justify-center">Data e hora locais:{currentLocalDateTime}</div>
+				<div className="flex justify-center">Data e hora Brasil:{currentBRDateTime}</div>
+				<div className="flex w-full justify-evenly align-center">
+					<Button variant="outlined" href="/">
+						Voltar
+					</Button>
+					<Button variant="contained" onClick={handleGetCurrentDateTime}>
+						Executar
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
